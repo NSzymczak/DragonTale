248a0ff0982728f9096d150ffc1a15e94653905c
Kamil Kozak <130604@student.uni.opole.pl>
2022-05-17 17:43:59 +0200

    Przeciwnicy, postać zadaje im obrażenia, obrażenia zadawane są postaci

--- src/Entity/FireBall.java
+++ src/Entity/FireBall.java
@@ -16,6 +16,8 @@ public class FireBall extends MapObject{
 
     public FireBall(TileMap tm, boolean right){
         super(tm);
+
+        facingRight = right;
         moveSpeed=3.8;
         if(right){
             dx=moveSpeed;
@@ -59,6 +61,9 @@ public class FireBall extends MapObject{
         checkTileMapCollision();
         setPosition(xtemp,ytemp);
 
+        if(dx==0 && !hit){
+            setHit();
+        }
         animation.update();
         if(hit  && animation.hasPlayedOnce()){
             remove=true;
@@ -67,10 +72,7 @@ public class FireBall extends MapObject{
     }
     public void draw(Graphics2D g){
         setMapPosition();
-        if(facingRight){
-            g.drawImage(animation.getImage(),(int)(x+xmap-width/2),(int)(y+ymap-height/2), null);
-        }else{
-            g.drawImage(animation.getImage(),(int)(x+xmap-width/2+width),(int)(y+ymap-height/2),-width,height,null);
-        }
+        super.draw(g);
     }
+
 }
--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -1,10 +1,15 @@
 package GameState;
 
+import Entity.Enemies.Slugger;
+import Entity.Enemy;
+import Entity.Explosion;
+import Entity.HUD;
 import Entity.Player;
 import Main.GamePanel;
 import TileMap.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class Level1State extends GameState{
 
@@ -12,6 +17,10 @@ public class Level1State extends GameState{
     private Background bg;
 
     private Player player;
+
+    private ArrayList<Enemy> enemies;
+    private ArrayList<Explosion> explosions;
+    private HUD hud;
 
     public Level1State(GameStateManager gsm){
         this.gsm=gsm;
@@ -30,6 +39,29 @@ public class Level1State extends GameState{
 
         player = new Player(tileMap);
         player.setPosition(100,100);
+
+        populateEnemies();
+
+        explosions = new ArrayList<Explosion>();
+
+        hud = new HUD(player);
+    }
+
+    private void populateEnemies(){
+        enemies = new ArrayList<Enemy>();
+        Slugger s;
+        Point[] points = new Point[]{
+          new Point(200,100),
+          new Point(860,200),
+          new Point(1525,200),
+          new Point(1680,200),
+          new Point(1800,200)
+        };
+        for(int i = 0; i < points.length; i++){
+            s = new Slugger(tileMap);
+            s.setPosition(points[i].x,points[i].y);
+            enemies.add(s);
+        }
     }
 
     @Override
@@ -37,6 +69,30 @@ public class Level1State extends GameState{
         //update
         player.update();
         tileMap.setPosition(GamePanel.WIDTH/2-player.getx(),GamePanel.HEIGHT/2-player.gety());
+        //moving background
+        //bg.setPosition(tileMap.getx(), tileMap.gety());
+
+        //attack enemies
+        player.checkAttack(enemies);
+
+        //update all enemies
+        for(int i = 0; i<enemies.size();i++){
+            Enemy e = enemies.get(i);
+            enemies.get(i).update();
+            if(enemies.get(i).isDead()){
+                enemies.remove(i);
+                i--;
+                explosions.add(new Explosion(e.getx(), e.gety()));
+            }
+        }
+
+        for(int i = 0; i <explosions.size();i++){
+            explosions.get(i).update();
+            if(explosions.get(i).shouldRemove()){
+                explosions.remove(i);
+                i--;
+            }
+        }
     }
 
     @Override
@@ -44,6 +100,14 @@ public class Level1State extends GameState{
         bg.draw(g);
         tileMap.draw(g);
         player.draw(g);
+        for(int i =0;i<enemies.size();i++){
+            enemies.get(i).draw(g);
+        }
+        for(int i = 0; i < explosions.size();i++){
+            explosions.get(i).setMapPosition((int)tileMap.getx(),(int)tileMap.gety())  ;
+            explosions.get(i).draw(g);
+        }
+        hud.draw(g);
     }
 
     @Override
